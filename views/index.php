f9df133c14fc3a7199cf1657fde60e9f9d87e715
lippekstro <fellipecastro13@hotmail.com>
2022-12-20 20:40:47 -0300

    primeiro commit

---
+++ views/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja de Celulares</title>
+</head>
+<body>
+    <header>
+        <!-- AQUI VAO OS ITENS DO CABEÇALHO DA PAGINA -->
+    </header>
+
+    <main>
+        <!-- AQUI VAO OS ITENS DO CONTEUDO PRINCIPAL DA PAGINA -->
+    </main>
+
+    <footer>
+        <!-- AQUI VAO OS ITENS DO RODAPE DA PAGINA -->
+    </footer>
+</body>
+</html>
