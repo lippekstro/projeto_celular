6aaf5fa3c45a33d0e07dc007bad9a267925aad5d
lippekstro <fellipecastro13@hotmail.com>
2022-12-22 20:50:27 -0300

    menu global

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -8,12 +8,39 @@
     <title>Loja de Celulares</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/carrossel.css">
+    <link rel="stylesheet" href="../css/cabecalho.css">
 
 </head>
 
 <body>
     <header>
         <!-- AQUI VAO OS ITENS DO CABEÇALHO DA PAGINA -->
+        <div class="container-pesquisa">
+            <div class="container-logo">
+                <img src="../img/logo-black-removebg-preview.png" alt="">
+            </div>
+
+            <div>
+                <form action="">
+                    <input type="search" name="" id="">
+                    <button type="submit">BUSCAR</button>
+                </form>
+            </div>
+
+            <div>
+                <span>carrinho</span>
+                <span>login</span>
+            </div>
+        </div>
+        <div class="menu-global">
+            <ul class="links-global">
+                <li><a href="">INICIO</a></li>
+                <li><a href="">CELULARES</a></li>
+                <li><a href="">SOBRE NÓS</a></li>
+                <li><a href="">SUPORTE</a></li>
+                <li><a href="">COMPARE</a></li>
+            </ul>
+        </div>
     </header>
 
     <main>
--- views/index.php
+++ views/index.php
@@ -1,41 +1,44 @@
 <?php
 require_once '../templates/cabecalho.php';
 ?>
+    <!-- container do carrossel -->
+<div class="container-carrossel">
+    <!-- container dos slides -->
+    <div class="slideshow-container">
 
-<!-- container do carrossel -->
-<div class="slideshow-container">
+        <!-- imagens com largura maxima com numero e texto de legenda -->
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="https://source.unsplash.com/random?landscape,mountain" style="width:100%">
+            <div class="text">Caption Text</div>
+        </div>
 
-    <!-- imagens com largura maxima com numero e texto de legenda -->
-    <div class="mySlides fade">
-        <div class="numbertext">1 / 3</div>
-        <img src="https://source.unsplash.com/random?landscape,mountain" style="width:100%">
-        <div class="text">Caption Text</div>
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="https://source.unsplash.com/random?landscape,night" style="width:100%">
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="https://source.unsplash.com/random?landscape,city" style="width:100%">
+            <div class="text">Caption Three</div>
+        </div>
+
+        <!-- botoes "proximo" e "anterior" -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
     </div>
+    <br>
 
-    <div class="mySlides fade">
-        <div class="numbertext">2 / 3</div>
-        <img src="https://source.unsplash.com/random?landscape,night" style="width:100%">
-        <div class="text">Caption Two</div>
+    <!-- os circulos -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
     </div>
-
-    <div class="mySlides fade">
-        <div class="numbertext">3 / 3</div>
-        <img src="https://source.unsplash.com/random?landscape,city" style="width:100%">
-        <div class="text">Caption Three</div>
-    </div>
-
-    <!-- botoes "proximo" e "anterior" -->
-    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
-<br>
 
-<!-- os circulos -->
-<div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-</div>
 
 <?php
 require_once '../templates/rodape.php';
