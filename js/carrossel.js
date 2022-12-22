a287a0e24edc5d4fda3d3b930cb467d6abc3072d
lippekstro <fellipecastro13@hotmail.com>
2022-12-22 19:59:10 -0300

    js do carrossel

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,28 +1,36 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1; // define o primeiro slide
+showSlides(slideIndex); //chama a funcao showslide no index em que estiver o slideindex
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// controle dos botoes proximo/anterior
+function plusSlides(n) { 
+  showSlides(slideIndex += n); //define de quantos em quantos slides ira passar
 }
 
-// Thumbnail image controls
+// controle de imagens
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex = n); //define qual slide deve ser exibido
 }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("mySlides"); //armazena a img
+  let dots = document.getElementsByClassName("dot"); //armazena a "bolinha"
   if (n > slides.length) {slideIndex = 1}
+  //se o "n" for maior que o tamanho do vetor de slides, retorne o index para 1 (volta pro primeiro slide)
   if (n < 1) {slideIndex = slides.length}
+  //se o "n" for menor que 1, retorne o index final(volta pro ultimo slide)
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none"; //oculta os slides
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", ""); 
+    //remove a classe das bolinhas
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block"; //exibe o slide
+  dots[slideIndex-1].className += " active"; // adiciona a classe as bolinhas
 }
+
+// troca slides automaticamente
+setInterval(()=>{ //funcao nao nomeada
+  showSlides(slideIndex += 1); //chama a funcao de exibicao de slides no index + 1
+}, 2000) //tempo de troca dos slides
--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loja de Celulares</title>
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/carrossel.css">
+
 </head>
 
 <body>
