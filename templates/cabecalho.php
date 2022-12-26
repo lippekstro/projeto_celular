c53e40f0a4d48927dacad97ef669c5e78c8dc39e
lippekstro <fellipecastro13@hotmail.com>
2022-12-26 20:02:35 -0300

    finalizei o menu global

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -6,9 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loja de Celulares</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/carrossel.css">
     <link rel="stylesheet" href="../css/cabecalho.css">
+
 
 </head>
 
@@ -17,19 +19,25 @@
         <!-- AQUI VAO OS ITENS DO CABEÇALHO DA PAGINA -->
         <div class="container-pesquisa">
             <div class="container-logo">
-                <img src="../img/logo-black-removebg-preview.png" alt="">
+                <a href="../views/index.php">
+                    <img src="../img/logo-white-removebg-preview.png" alt="">
+                </a>
             </div>
 
             <div>
                 <form action="">
-                    <input type="search" name="" id="">
-                    <button type="submit">BUSCAR</button>
+                    <div class="caixa_busca">
+                        <input type="search" name="" id="">
+                        <button class="botao_busca" type="submit">
+                            <span class="material-symbols-outlined">search</span>
+                        </button>
+                    </div>
                 </form>
             </div>
 
-            <div>
-                <span>carrinho</span>
-                <span>login</span>
+            <div class="carrinho_login">
+                <a href=""><span class="material-symbols-outlined">shopping_cart</span></a>
+                <a href=""><span class="material-symbols-outlined">login</span></a>
             </div>
         </div>
         <div class="menu-global">
