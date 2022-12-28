35c50b76ac53dd435efa7168fd48209ab29532e5
lippekstro <fellipecastro13@hotmail.com>
2022-12-28 20:45:03 -0300

    pequenas correcoes icones

--- views/cadastro.php
+++ views/cadastro.php
@@ -7,45 +7,40 @@ require_once '../templates/cabecalho.php';
         <img src="../img/logo-black-removebg-preview.png" alt="">
     </div>
     <div class="">
-        <form action="" class="container-cadastro">
+        <form action="" class="container-cadastro" method="POST">
             <div class="elemento-form-cadastro">
-                <label for="nome">Nome</label>
-                <input type="text" name="nome" id="nome">
+                <label for="nome"><span class="material-symbols-outlined">person</span></label>
+                <input type="text" name="nome" id="nome" placeholder="Nome">
             </div>
 
             <div class="elemento-form-cadastro">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email">
+                <label for="email"><span class="material-symbols-outlined">mail</span></label>
+                <input type="email" name="email" id="email" placeholder="E-mail">
             </div>
 
             <div class="elemento-form-cadastro">
-                <label for="telefone">Telefone</label>
-                <input type="tel" name="telefone" id="telefone">
-
+                <label for="telefone"><span class="material-symbols-outlined">call</span></label>
+                <input type="tel" name="telefone" id="telefone" placeholder="Telefone">
             </div>
 
             <div class="elemento-form-cadastro">
-                <label for="endereco">Endereço</label>
-                <input type="text" name="endereco" id="endereco">
-
+                <label for="endereco"><span class="material-symbols-outlined">home</span></label>
+                <input type="text" name="endereco" id="endereco" placeholder="Endereço">
             </div>
 
             <div class="elemento-form-cadastro">
-                <label for="senha">Senha</label>
-                <input type="password" name="senha" id="senha">
-
+                <label for="senha"><span class="material-symbols-outlined">key</span></label>
+                <input type="password" name="senha" id="senha" placeholder="Senha">
             </div>
 
             <div class="elemento-form-cadastro">
-                <label for="confirma_senha">Confirmar Senha</label>
-                <input type="password" name="confirma_senha" id="confirma_senha">
-
+                <label for="confirma_senha"><span class="material-symbols-outlined">key</span></label>
+                <input type="password" name="confirma_senha" id="confirma_senha" placeholder="Confirmar Senha">
             </div>
 
             <div class="elemento-form-cadastro">
                 <button type="submit">Cadastrar</button>
             </div>
-
         </form>
     </div>
 </div>
--- views/login.php
+++ views/login.php
@@ -6,17 +6,20 @@ require_once '../templates/cabecalho.php';
     <form action="" method="POST" class="form-login">
         <div class="container-login">
             <div class="elemento_form_login">
-                <label for="login">Login</label>
-                <input type="text" name="login" id="login">
+                <label for="login"><span class="material-symbols-outlined">person</span></label>
+                <input type="text" name="login" id="login" placeholder="Login">
             </div>
 
             <div class="elemento_form_login">
-                <label for="senha">Senha</label>
-                <input type="password" name="senha" id="senha">
+                <label for="senha"><span class="material-symbols-outlined">key</span></label>
+                <input type="password" name="senha" id="senha" placeholder="Senha">
             </div>
 
             <div class="elemento_form_login">
                 <a href="cadastro.php"><span>Se você não tem conta, cadastre-se aqui</span></a>
+            </div>
+
+            <div class="elemento_form_login">
                 <a href=""><span>Esqueceu a senha?</span></a>
             </div>
 
