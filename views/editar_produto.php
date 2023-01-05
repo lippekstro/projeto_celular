32ecc74098f60519cad68ababaa302eca229f148
lippekstro <fellipecastro13@hotmail.com>
2023-01-05 19:58:38 -0300

    bug do editar arrumado

--- views/editar_produto.php
+++ views/editar_produto.php
@@ -3,18 +3,18 @@ require_once '../templates/cabecalho.php';
 
 try {
     $id_produto = $_GET['id_produto'];
-    $lista = Produto::listarPeloId($id_produto);
+    $celular = Produto::listarPeloId($id_produto);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
+
 
 ?>
 
 <div>
     <form action="../controllers/editar_produto_controller.php" method="post" enctype="multipart/form-data" class="container-cadastro">
-        <?php foreach ($lista as $celular) : ?>
             <div>
-                <input type="number" name="id_produto" id="id_produto" value="<?= $celular['id_produto'] ?>" hidden>
+                <input type="number" name="id_produto" id="id_produto" value="<?= $celular->id_produto ?>" hidden>
             </div>
 
             <div class="elemento-form-cadastro">
@@ -24,33 +24,32 @@ try {
 
             <div class="elemento-form-cadastro">
                 <label for="nome"><span class="material-symbols-outlined">smartphone</span></label>
-                <input type="text" name="nome" id="nome" value="<?= $celular['nome'] ?>">
+                <input type="text" name="nome" id="nome" value="<?= $celular->nome ?>">
             </div>
 
             <div class="elemento-form-cadastro">
                 <label for="descricao"><span class="material-symbols-outlined">description</span></label>
-                <textarea name="descricao" id="descricao" cols="30" rows="10"><?= $celular['descricao'] ?></textarea>
+                <textarea name="descricao" id="descricao" cols="30" rows="10"><?= $celular->descricao ?></textarea>
             </div>
 
             <div class="elemento-form-cadastro">
                 <label for="preco"><span class="material-symbols-outlined">sell</span></label>
-                <input type="number" name="preco" id="preco" value="<?= $celular['preco'] ?>">
+                <input type="number" name="preco" id="preco" value="<?= $celular->preco ?>">
             </div>
 
             <div class="elemento-form-cadastro">
                 <label for="fabricante"><span class="material-symbols-outlined">factory</span></label>
-                <input type="text" name="fabricante" id="fabricante" value="<?= $celular['fabricante'] ?>">
+                <input type="text" name="fabricante" id="fabricante" value="<?= $celular->fabricante ?>">
             </div>
 
             <div class="elemento-form-cadastro">
                 <label for="estoque"><span class="material-symbols-outlined">tag</span></label>
-                <input type="number" name="estoque" id="estoque" value="<?= $celular['estoque'] ?>">
+                <input type="number" name="estoque" id="estoque" value="<?= $celular->estoque ?>">
             </div>
 
             <div class="elemento-form-cadastro">
                 <button type="submit">Atualizar</button>
             </div>
-        <?php endforeach; ?>
     </form>
 </div>
 
